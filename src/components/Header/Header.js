c93622a7c25390c166afe4f2d23b4b753b2c93e7
Sanjid <sanjidk24@gmail.com>
2022-03-26 00:21:40 +0600

    api json create

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-div'>
+      <p className='header-title'>Food Lover House</p>
+      <p className='header-sub-title'>Enjoy Deshi Food</p>
+
+    </div>
+  );
+};
+
+export default Header;
