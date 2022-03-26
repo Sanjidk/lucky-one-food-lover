8c0b7e4ba5a1a826adc1ef3c07c162ebbedc0369
Sanjid <sanjidk24@gmail.com>
2022-03-26 19:10:23 +0600

    question answer done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Foods from './components/Foods/Foods';
 import Header from './components/Header/Header';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Foods></Foods>
+      <Question></Question>
 
     </div>
   );
---
+++ src/components/Question/Question.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Question = () => {
+  return (
+    <div className='mt-5'>
+      <h1 className='fw-bold text-center'>Question And Answer</h1>
+      <div className='m-3'>
+        <div className="row">
+          <div className="col-lg-6 mt-3">
+            <div className="card border-0 shadow-lg">
+              <div className="card-body">
+                <h5 className="card-title fw-bold"> 1. How React Works . ???</h5>
+                <p className="card-text fw-bold"> ReactJS is an open-source JavaScript library. It is used for building user interfaces. Specifically it is used for single-page applications. It actually works with declarative code. Fake HTML code can run in JS file. So, no need to extra HTML file. It works for specially in Web application and Mobile app. It has a lot of new library file. So, i just know thats way.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 mt-3">
+            <div className="card border-0 shadow-lg">
+              <div className="card-body">
+                <h5 className="card-title fw-bold"> 2. Different Between PROPS vs STATE . ???</h5>
+                <p className="card-text fw-bold"> 
+                1. Props are immutable but State are mutable. <br />
+                2. By Props, properties can pass from parent compunents but By State, it can define in the component itself. <br />
+                3. Props can not be modified but State can be modified by using this.setState. <br />
+                4. Props are read-only but State changes can be asynchronous. <br />
+                5. Props remain static but State is updated by the object.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+  );
+};
+
+export default Question;
